917c475d107854c34d69374736640205b713e8b4
Helminafan <nafan2332@gmail.com>
2023-11-17 10:09:13 +0800

    edit bash

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker build -t Helminafan/item-app:v1 .
+docker build -t helminafan/item-app:v1 .
 
 docker images
 
-docker tag Helminafan/item-app:v1 docker.pkg.github.com/Helminafan/a433-microservices/item-app:v1
+docker tag helminafan/item-app:v1 docker.pkg.github.com/helminafan/a433-microservices/item-app:v1
 
-docker login docker.pkg.github.com -u Helminafan -p PAT
+docker login docker.pkg.github.com -u helminafan -p PAT
 
-docker push docker.pkg.github.com/Helminafan/a433-microservices/item-app:v1
+docker push docker.pkg.github.com/helminafan/a433-microservices/item-app:v1
